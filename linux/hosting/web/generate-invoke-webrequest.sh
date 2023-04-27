8ad27141e663d27cb98a503fd4b0653db9409ec1
root <root@kali>
2023-04-27 13:44:19 +0100

    Added script to create invoke webrequest

--- linux/hosting/web/generate-invoke-webrequest.sh
+++ linux/hosting/web/generate-invoke-webrequest.sh
@@ -5,14 +5,22 @@ echo "Please enter the URL:"
 read url
 
 # Check if the URL is valid
-if [[ ! $url =~ ^https?:// ]]; then
-    echo "Invalid URL. Please enter a URL starting with 'http://' or 'https://'"
+if [[ ! $url =~ ^http:// ]]; then
+    echo "Invalid URL. Please enter a URL starting with 'http://'"
     exit 1
 fi
 
-# Generate the PowerShell Invoke-WebRequest command
-powershell_command="powershell.exe -Command \"Invoke-WebRequest -Uri '$url'\""
+# Generate the PowerShell 2.0 command using System.Net.WebClient
+powershell_2_command="powershell.exe -Command \"\$wc = New-Object System.Net.WebClient; \$wc.DownloadFile('$url', [System.IO.Path]::GetFileName('$url'))\""
 
-# Print the generated command
-echo "Generated PowerShell command:"
-echo "$powershell_command"
+# Generate the PowerShell 5.0 command using Invoke-WebRequest
+powershell_5_command="powershell.exe -Command \"Invoke-WebRequest -Uri '$url' -OutFile ([System.IO.Path]::GetFileName('$url'))\""
+
+# Print the generated commands
+echo "Generated PowerShell commands:"
+echo ""
+echo "For PowerShell 2.0:"
+echo "$powershell_2_command"
+echo ""
+echo "For PowerShell 5.0 or later:"
+echo "$powershell_5_command"
