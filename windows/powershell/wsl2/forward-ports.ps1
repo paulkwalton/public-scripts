75566d03ae63471b91beed3e9e326b6e4fc3c416
paulkwalton <43542501+paulkwalton@users.noreply.github.com>
2023-05-25 13:44:33 +0100

    Update forward-ports.ps1

--- windows/powershell/wsl2/forward-ports.ps1
+++ windows/powershell/wsl2/forward-ports.ps1
@@ -1,28 +1,34 @@
 # Define the ports you want to forward
-$portsToForward = @(80, 443, 4444, 8080)
+$portsToForward = @(80, 443, 4444, 8080, 8888)
 
 # Get the WSL2 Linux host IP address
 $wslIp = (wsl hostname -I).Trim()
-
-# Get the Windows host IP address
-$windowsIp = (Get-NetIPAddress -AddressFamily IPv4 -InterfaceAlias "vEthernet (WSL)" -AddressState Preferred).IPAddress
 
 # Reset all existing port forwarding rules
 Write-Host "Resetting all existing rules..."
 netsh interface portproxy reset
 
-# Configure port forwarding
-ForEach ($port in $portsToForward) {
-    Write-Host "Configuring port forwarding for port: $port"
+# Get all Windows IPv4 addresses
+$windowsIPs = Get-NetIPAddress -AddressFamily IPv4 | Where-Object { $_.InterfaceAlias -notlike "*Loopback*" -and $_.IPAddress -notlike "169.*" -and $_.IPAddress -notlike "127.*"}
 
-    # Add a new port forwarding rule
-    Write-Host "Adding new rule..."
-    $addResult = netsh interface portproxy add v4tov4 listenport=$port listenaddress=$windowsIp connectport=$port connectaddress=$wslIp
-    Write-Host $addResult
+ForEach ($windowsIP in $windowsIPs) {
+
+    $currentWindowsIp = $windowsIP.IPAddress
+
+    # Configure port forwarding
+    ForEach ($port in $portsToForward) {
+        Write-Host "Configuring port forwarding for port: $port"
+
+        # Add a new port forwarding rule
+        Write-Host "Adding new rule for $currentWindowsIp..."
+        $addResult = netsh interface portproxy add v4tov4 listenport=$port listenaddress=$currentWindowsIp connectport=$port connectaddress=$wslIp
+        Write-Host $addResult
+    }
 }
 
 # Print the port forwarding rules
 Write-Host "Current port forwarding rules:"
 netsh interface portproxy show v4tov4
+
 
 
