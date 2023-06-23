5869a6e06ba0de8aa6810563c575d2c935d35627
paulkwalton <43542501+paulkwalton@users.noreply.github.com>
2023-06-23 11:42:07 +0100

    Update w10.ps1

--- windows/build-scripts/w10.ps1
+++ windows/build-scripts/w10.ps1
@@ -114,6 +114,7 @@ $KaliLinuxFile = "C:\temp\kali-linux.AppxBundle"
 Invoke-WebRequest -Uri $KaliLinuxUrl -OutFile $KaliLinuxFile -UseBasicParsing -TimeoutSec 1800
 
 # Install Kali Linux
+wsl --set-default-version 2
 Add-AppxPackage -Path $KaliLinuxFile
 
 # Self-deleting
@@ -135,8 +136,6 @@ $PostRestartScriptContent | Out-File -FilePath 'C:\temp\PostRestartScript.ps1'
 
 # Copy the PostRestartScript.ps1 to the Desktop
 Copy-Item -Path 'C:\temp\PostRestartScript.ps1' -Destination "$([Environment]::GetFolderPath('Desktop'))\RunMeAfterReboot.ps1"
-
-wsl --set-version kali-linux 2
 
 # Restart the computer
 Restart-Computer
