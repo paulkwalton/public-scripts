60fa69db96df0abb5ade4b99adb680602b905022
paulkwalton <43542501+paulkwalton@users.noreply.github.com>
2023-06-28 11:44:26 +0100

    Update ICMP-Sweep.sh

--- ICS/IDS-Testing/ICMP-Sweep.sh
+++ ICS/IDS-Testing/ICMP-Sweep.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 
-# Prompt the user to enter the IP or subnet to scan
-read -p "Enter the IP or subnet to scan (in CIDR notation): " SUBNET
-
 # Get the IP address and hostname of the current machine
 IP_ADDRESS=$(hostname -I | cut -d' ' -f1)
 HOSTNAME=$(hostname)
 
-# Print the IP address, hostname, and subnet
+# Print the IP address and hostname
 echo "Your Hostname: $HOSTNAME"
 echo "Your IP Address: $IP_ADDRESS"
+
+# Prompt the user to enter the IP or subnet to scan
+read -p "Enter the IP or subnet to scan (in CIDR notation): " SUBNET
+
+# Print the subnet to be scanned
 echo "Scanning Subnet: $SUBNET"
 
-# Perform a ping sweep of the subnet
-nmap -sn -n -v $SUBNET
+# Perform a TCP port scan of the top 1000 ports on the subnet
+nmap -F $SUBNET
 
 # Print the current date and time
 echo "Script ran at $(date)"
+
