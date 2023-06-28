bd24882f3c195f32428fd63010f0254e4714d034
paulkwalton <43542501+paulkwalton@users.noreply.github.com>
2023-06-28 08:48:26 +0100

    Create port-scan.sh

---
+++ ICS/IDS-Testing/port-scan.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Get the IP address of the current machine
+IP_ADDRESS=$(hostname -I | cut -d' ' -f1)
+
+# Get the subnet
+SUBNET=$(echo $IP_ADDRESS | cut -d'.' -f1-3)
+
+# Scan the subnet
+for i in $(seq 1 254); do
+    nmap -sS $SUBNET.$i
+done
