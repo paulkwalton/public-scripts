7e694d47f596935f1497558e7e0ed1f549101ccb
paulkwalton <43542501+paulkwalton@users.noreply.github.com>
2023-06-28 12:02:00 +0100

    Update Scan-Connect-RDP.sh

--- ICS/IDS-Testing/Scan-Connect-RDP.sh
+++ ICS/IDS-Testing/Scan-Connect-RDP.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 
-# Get the IP address of the current machine
+# Get the IP address and hostname of the current machine
 IP_ADDRESS=$(hostname -I | cut -d' ' -f1)
+HOSTNAME=$(hostname)
 
-# Get the subnet
-SUBNET=$(echo $IP_ADDRESS | cut -d'.' -f1-3)
+# Print the IP address and hostname
+echo "Your Hostname: $HOSTNAME"
+echo "Your IP Address: $IP_ADDRESS"
 
-# Scan the subnet for open RDP ports and launch RDP connections
-for i in $(seq 1 254); do
-    if nmap -p 3389 $SUBNET.$i | grep -q "3389/tcp open"; then
-        rdesktop $SUBNET.$i &
-    fi
-done
+# Prompt the user to enter the IP or subnet to scan
+read -p "Enter the IP or subnet to scan (in CIDR notation): " SUBNET
+
+# Print the subnet to be scanned
+echo "Scanning Subnet: $SUBNET"
+
+# Scan the subnet for open RDP ports and attempt to connect
+nmap -p 3389 $SUBNET | grep -q "3389/tcp open" && xfreerdp /v:$SUBNET
+
+# Print the current date and time
+echo "Script ran at $(date)"
