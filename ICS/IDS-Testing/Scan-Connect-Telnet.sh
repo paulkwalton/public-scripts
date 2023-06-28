611a85c9e2bcdeaa3bf13c903fe342c97247e0f4
paulkwalton <43542501+paulkwalton@users.noreply.github.com>
2023-06-28 13:02:06 +0100

    Update Scan-Connect-Telnet.sh

--- ICS/IDS-Testing/Scan-Connect-Telnet.sh
+++ ICS/IDS-Testing/Scan-Connect-Telnet.sh
@@ -1,14 +1,32 @@
 #!/bin/bash
 
-# Get the IP address of the current machine
+# Get the IP address and hostname of the current machine
 IP_ADDRESS=$(hostname -I | cut -d' ' -f1)
+HOSTNAME=$(hostname)
 
-# Get the subnet
-SUBNET=$(echo $IP_ADDRESS | cut -d'.' -f1-3)
+# Print the IP address and hostname
+echo "Your Hostname: $HOSTNAME"
+echo "Your IP Address: $IP_ADDRESS"
 
-# Scan the subnet for open Telnet ports and launch Telnet connections
-for i in $(seq 1 254); do
-    if nmap -p 23 $SUBNET.$i | grep -q "23/tcp open"; then
-        gnome-terminal -- telnet $SUBNET.$i &
-    fi
+# Prompt the user to enter the IP or subnet to scan
+read -p "Enter the subnet to scan (in CIDR notation): " SUBNET
+
+# Print the subnet to be scanned
+echo "Scanning Subnet: $SUBNET"
+
+# Scan the subnet for open Telnet ports and attempt to connect to each open port
+nmap -p 23 --open -oG - $SUBNET | awk '/Up$/{print $2}' | while read line; do
+    echo "Attempting to connect to $line via Telnet"
+    expect -c "
+    spawn telnet $line
+    expect \"login:\"
+    send \"fakeuser\r\"
+    expect \"Password:\"
+    send \"fakepassword\r\"
+    expect eof
+    "
 done
+
+# Print the current date and time
+echo "Script ran at $(date)"
+
