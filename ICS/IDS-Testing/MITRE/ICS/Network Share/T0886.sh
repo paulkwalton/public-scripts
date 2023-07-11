bcea3e1f53e7bbb781edd936a1214690ca584ca9
paulkwalton <43542501+paulkwalton@users.noreply.github.com>
2023-07-11 13:48:18 +0100

    Update T0886.sh

--- ICS/IDS-Testing/MITRE/ICS/Network Share/T0886.sh
+++ ICS/IDS-Testing/MITRE/ICS/Network Share/T0886.sh
@@ -1,1 +1,20 @@
+#!/bin/bash
+
+# Prompt the user to enter the file path to upload
+read -p "Enter the file path to upload: " FILE_PATH
+
+# Prompt for the SMB share details
+read -p "Enter the remote SMB server IP: " SMB_SERVER
+read -p "Enter the remote SMB share name: " SMB_SHARE
+
+# Check if the file exists and upload it
+if [ -f "$FILE_PATH" ]; then
+    echo "Uploading file: $FILE_PATH"
+    smbclient //$SMB_SERVER/$SMB_SHARE -N -c "put $FILE_PATH"
+else
+    echo "File does not exist: $FILE_PATH"
+fi
+
+# Print the current date and time
+echo "Script ran at $(date)"
 
