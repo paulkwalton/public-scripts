ef1978f2c8698a2a1a3dbe02e8ceb3ec20c9de29
paulkwalton <43542501+paulkwalton@users.noreply.github.com>
2023-07-11 15:08:18 +0100

    Create ms08-67.sh

---
+++ ICS/IDS-Testing/ms08-67.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# This script is designed to scan a target system for the MS08-067 vulnerability
+# using the ms08_067_netapi module in Metasploit. 
+
+read -p "Enter target IP: " target_ip
+
+# Creating Metasploit rc file
+cat > ms08_067_scan.rc <<EOF
+use auxiliary/scanner/smb/smb_ms08_067
+set RHOSTS $target_ip
+run
+exit
+EOF
+
+# Running Metasploit with the created rc file
+msfconsole -r ms08_067_scan.rc
