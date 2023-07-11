5c02f477ed93a3998996678282e9c069bc6de701
paulkwalton <43542501+paulkwalton@users.noreply.github.com>
2023-07-11 13:42:25 +0100

    Update T0811.sh

--- ICS/IDS-Testing/MITRE/ICS/Network Share/T0811.sh
+++ ICS/IDS-Testing/MITRE/ICS/Network Share/T0811.sh
@@ -1,1 +1,24 @@
+#!/bin/bash
 
+# Get the IP address and hostname of the current machine
+IP_ADDRESS=$(hostname -I | cut -d' ' -f1)
+HOSTNAME=$(hostname)
+
+# Print the IP address and hostname
+echo "Your Hostname: $HOSTNAME"
+echo "Your IP Address: $IP_ADDRESS"
+
+# Prompt the user to enter the IP or subnet to scan
+read -p "Enter the subnet to scan (in CIDR notation): " SUBNET
+
+# Print the subnet to be scanned
+echo "Scanning Subnet: $SUBNET for open SMB shares"
+
+# Scan the subnet for open SMB ports and list shares
+nmap -p 445 --open -oG - $SUBNET | awk '/Up$/{print $2}' | while read line; do
+    echo "Open SMB shares at $line"
+    smbclient -N -L \\$line
+done
+
+# Print the current date and time
+echo "Script ran at $(date)"
