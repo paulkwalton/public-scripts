d0af429964e3bcb971c59082184aea6783c0ae25
paulkwalton <43542501+paulkwalton@users.noreply.github.com>
2023-09-05 09:26:55 +0100

    Create enumerate-ad.ps1

---
+++ windows/enumeration/powerview/enumerate-ad.ps1
@@ -0,0 +1,80 @@
+ # Declare variables for domain and domain controller IP address
+$domain = "hacked.local"
+$domainController = "192.168.15.128"
+
+# Enumerate any domain trusts
+Write-Host "Enumerating Domain Trusts..." -ForegroundColor Red
+Start-Sleep -Seconds 2
+Get-NetDomainTrust -Domain $domain -DomainController $domainController
+Read-Host "Press Enter to continue..."
+Write-Host "`n"
+
+# Enumerate domain controllers
+Write-Host "Enumerating Domain Controllers..." -ForegroundColor Red
+Start-Sleep -Seconds 2
+Get-DomainController -Domain $domain -DomainController $domainController
+Read-Host "Press Enter to continue..."
+Write-Host "`n"
+
+# Enumerate domain policy
+Write-Host "Enumerating Domain Policy..." -ForegroundColor Red
+Start-Sleep -Seconds 2
+Get-DomainPolicy -Domain $domain -DomainController $domainController
+Read-Host "Press Enter to continue..."
+Write-Host "`n"
+
+# Find all Domain Admins with name and description
+Write-Host "Finding all Domain Admins..." -ForegroundColor Red
+Start-Sleep -Seconds 2
+Get-DomainGroupMember -Identity 'Domain Admins' -Domain $domain -DomainController $domainController | Select-Object MemberDistinguishedName
+Read-Host "Press Enter to continue..."
+Write-Host "`n"
+
+# Find all Enterprise Admins with name and description
+Write-Host "Finding all Enterprise Admins..." -ForegroundColor Red
+Start-Sleep -Seconds 2
+Get-DomainGroupMember -Identity 'Enterprise Admins' -Domain $domain -DomainController $domainController | Select-Object MemberDistinguishedName
+Read-Host "Press Enter to continue..."
+Write-Host "`n"
+
+# Find all user groups with name and description
+Write-Host "Finding all User Groups with Name and Description..." -ForegroundColor Red
+Start-Sleep -Seconds 2
+Get-NetGroup -Domain $domain -DomainController $domainController | Select-Object samaccountname, admincount, description
+Read-Host "Press Enter to continue..."
+Write-Host "`n"
+
+# Find any users with the SID bit set
+Write-Host "Finding Users with SID bit set..." -ForegroundColor Red
+Start-Sleep -Seconds 2
+Get-NetUser -LDAPFilter '(sidHistory=*)' -Domain $domain -DomainController $domainController
+Read-Host "Press Enter to continue..."
+Write-Host "`n"
+
+# Find Kerberoastable Users
+Write-Host "Finding Kerberoastable Users..." -ForegroundColor Red
+Start-Sleep -Seconds 2
+Get-NetUser -SPN -Domain $domain -DomainController $domainController
+Read-Host "Press Enter to continue..."
+Write-Host "`n"
+
+# Find ASREP Roastable Users
+Write-Host "Finding ASREP Roastable Users..." -ForegroundColor Red
+Start-Sleep -Seconds 2
+Get-NetUser -PreAuthNotRequired -Domain $domain -DomainController $domainController
+Read-Host "Press Enter to continue..."
+Write-Host "`n"
+
+# Find All Domain Users and Export into a text file for password attacks
+Write-Host "Finding All Users and Export into a text file for password attacks" -ForegroundColor Red
+Start-Sleep -Seconds 2
+ Get-DomainUser -Domain $domain -DomainController $domainController | select UserPrincipalName | Out-File -Append "$env:USERPROFILE\Documents\DomainUsers.txt"
+Read-Host "Press Enter to continue..."
+Write-Host "`n"
+
+# Find All local admin users on every endpoint
+Write-Host "Finding All local admin users" -ForegroundColor Red
+Start-Sleep -Seconds 2
+  Find-DomainLocalGroupMember -verbose -Domain $domain -DomainController $domainController
+Read-Host "Press Enter to continue..."
+Write-Host "`n" 
