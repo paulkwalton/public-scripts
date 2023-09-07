68be659a4d8db7bc6205caa8420df0811fda3181
paulkwalton <43542501+paulkwalton@users.noreply.github.com>
2023-09-07 09:54:52 +0100

    Update fake-ldap.sh

--- linux/hosting/ldap/fake-ldap.sh
+++ linux/hosting/ldap/fake-ldap.sh
@@ -1,2 +1,5 @@
 sudo apt-get update && sudo apt-get -y install slapd ldap-utils && sudo systemctl enable slapd
 sudo dpkg-reconfigure -p low slapd
+echo -e "dn: cn=config\nreplace: olcSaslSecProps\nolcSaslSecProps: noanonymous,minssf=0,passcred" > /opt/olcSaslSecProps.ldif
+sudo ldapmodify -Y EXTERNAL -H ldapi:// -f /opt/olcSaslSecProps.ldif && sudo service slapd restart
+
