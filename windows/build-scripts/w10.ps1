4209709e355544a180663908c416a4a00aac3e62
paulkwalton <43542501+paulkwalton@users.noreply.github.com>
2023-09-07 10:36:18 +0100

    Update w10.ps1

--- windows/build-scripts/w10.ps1
+++ windows/build-scripts/w10.ps1
@@ -100,6 +100,7 @@ git clone https://github.com/GhostPack/SafetyKatz.git c:\tools\safetykatz
 git clone https://github.com/paulkwalton/public-scripts.git c:\tools\public-scripts
 git clone https://github.com/antonioCoco/RogueWinRM.git c:\tools\roguewinrm
 git clone https://github.com/itm4n/PrivescCheck.git c:\tools\PrivescCheck
+git clone https://github.com/wavestone-cdt/powerpxe.git c:\tools\powerpxe
 # Restart Windows & rename Windows
 Write-Output "Rename & Reboot Windows"
 Rename-Computer -NewName "WinTak"
